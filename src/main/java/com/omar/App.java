1fa3a508aa804b2b3b5ae663cbae3e5c4772f2a3
Omar <martinez.trujillo.omar@gmail.com>
2021-01-06 17:40:37 +0100

    refactored method names and added docs

--- src/main/java/com/omar/App.java
+++ src/main/java/com/omar/App.java
@@ -12,15 +12,19 @@ public class App {
             //MealyMachine machine = new MealyMachine(args[0]);
             MealyMachine machine = new MealyMachine("configPath");
 
-            loadHeader(machine);
-            startChecking(machine);
+            printHeader(machine);
+            startMachine(machine);
 
         } catch (Exception e) {
             System.out.println("Valid input.");
         }
     }
 
-    private static void loadHeader(MealyMachine machine) {
+    /**
+     * Prints instructions on how to use the software and the valid symbols to use
+     * @param machine the machine from which the valid symbols will be taken
+     */
+    private static void printHeader(MealyMachine machine) {
         System.out.println("Enter words with the accepted symbols to see if they are valid.");
         System.out.println("Accepted Symbols:");
 
@@ -31,7 +35,11 @@ public class App {
         System.out.println("Enter exit to exit the program");
     }
 
-    public static void startChecking(MealyMachine machine) {
+    /**
+     * Repeatedly requests the user for input that will be verified by the MealyMachine
+     * @param machine the machine that will check the user input
+     */
+    public static void startMachine(MealyMachine machine) {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine();
